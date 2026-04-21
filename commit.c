bca2d82292146cb47679dc83e284fc544add334e
Aayushman Singh <aayushmansingh4167@gmail.com>
2026-04-21 11:10:21 +0530

    Phase 4 Commit 5: Error fixing

--- commit.c
+++ commit.c
@@ -209,7 +209,7 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
         commit.parent = parent_id;
         commit.has_parent = 1;
     } else {
-        commit.has_parent = 0; 
+        commit.has_parent = 0;
     }
 
     snprintf(commit.author, sizeof(commit.author), "%s", pes_author());
@@ -217,5 +217,22 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     commit.timestamp = (uint64_t)time(NULL);
 
     snprintf(commit.message, sizeof(commit.message), "%s", message);
+
+    void *data;
+    size_t len;
+    if (commit_serialize(&commit, &data, &len) != 0) {
+        return -1;
+    }
+
+    if (object_write(OBJ_COMMIT, data, len, commit_id_out) != 0) {
+        free(data);
+        return -1;
+    }
+    free(data);
+
+    if (head_update(commit_id_out) != 0) {
+        return -1;
+    }
+
     return 0;
 }
